c205a541f182cf5400c49068ad97f4c194d386e5
biancazrusu <bianca.zrusu@gmail.com>
2017-04-24 13:53:37 +0300

    *

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ host='127.0.0.1'
 
 @app.route('/')
 def index():
-    return render_template('board.html')
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=80,debug=False)
