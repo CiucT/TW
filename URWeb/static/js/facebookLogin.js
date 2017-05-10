b85c3897674176354f3a7a0644e98d5c032c75d1
biancazrusu <bianca.zrusu@gmail.com>
2017-05-10 17:05:05 +0300

    facebookLogin.js
    moved the script for facebook in facebookLogin.js

--- URWeb/static/js/facebookLogin.js
+++ URWeb/static/js/facebookLogin.js
@@ -1,5 +1,6 @@
 FB.init({
   appId: '419512185089420', // Your app id
+  apiKey: '5646cecf9effba7e810d28c577248570',
   channelUrl : 'http://localhost', // Your channel url
   xfbml: true,
   status: true,
@@ -16,4 +17,14 @@ function checkLoginStatus(response) {
     } else {
         document.getElementById("fb").checked = false
     }
+}
+
+FB.Event.subscribe('auth.login', function () {
+          window.location.href = "{{ url_for('board') }}";
+ });
+
+function LogoutFacebook(){
+FB.logout(function(response) {
+  // user is now logged out
+});
 }
