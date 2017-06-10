90f3167f3a587424ca0f9e1ddcb5c0865cfd8c37
biancazrusu <bianca.zrusu@gmail.com>
2017-06-10 12:55:26 +0300

    database table sugestii_locatie

--- urweb_sugestii_locatie.sql
+++ urweb_sugestii_locatie.sql
@@ -16,31 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `facebook_users`
+-- Table structure for table `sugestii_locatie`
 --
 
-DROP TABLE IF EXISTS `facebook_users`;
+DROP TABLE IF EXISTS `sugestii_locatie`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `facebook_users` (
-  `id` varchar(50) NOT NULL,
-  `first_name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL,
-  `e_mail` varchar(50) DEFAULT NULL,
-  `likes` varchar(50) DEFAULT NULL,
-  `profile_pic` varchar(100) DEFAULT NULL,
-  UNIQUE KEY `id` (`id`)
+CREATE TABLE `sugestii_locatie` (
+  `user_id` varchar(200) NOT NULL,
+  `place_id` varchar(200) DEFAULT NULL,
+  `descriere` varchar(300) DEFAULT NULL,
+  `adresa` varchar(300) DEFAULT NULL,
+  `latitudine` float DEFAULT NULL,
+  `longitudine` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `facebook_users`
+-- Dumping data for table `sugestii_locatie`
 --
 
-LOCK TABLES `facebook_users` WRITE;
-/*!40000 ALTER TABLE `facebook_users` DISABLE KEYS */;
-INSERT INTO `facebook_users` VALUES ('1478509985553171','Bianca','Rusu','rusu.bianca16@yahoo.com',NULL,'//graph.facebook.com/1478509985553171/picture');
-/*!40000 ALTER TABLE `facebook_users` ENABLE KEYS */;
+LOCK TABLES `sugestii_locatie` WRITE;
+/*!40000 ALTER TABLE `sugestii_locatie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sugestii_locatie` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -52,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-10 12:54:45
+-- Dump completed on 2017-06-10 12:54:44
